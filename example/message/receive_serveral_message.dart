804106875c4853685de910e93c91b721a10881b6
Gleb Batykov <gleb.batykov@gmail.com>
2022-01-13 01:32:29 +0300

    Adds new capabilities for handling actor messages
    Adds the ability to add an actor message handler for a specific number of messages.
    
    Adds example.

---
+++ example/message/receive_serveral_message.dart
@@ -0,0 +1,34 @@
+import 'package:pedantic/pedantic.dart';
+import 'package:theater/theater.dart';
+
+// Create actor class
+class TestActor extends UntypedActor {
+  // Override onStart method which will be executed at actor startup
+  @override
+  Future<void> onStart(UntypedActorContext context) async {
+    // Set handler to only 5 following messages of type String, then print 'Hello, from test actor!' message.
+    // Method [receiveSeveral] return Future and you can wait or not wait him.
+    unawaited(context.receiveSeveral<String>(5, (message) async {
+      print(message);
+    }).then((_) {
+      print('Hello, from test actor!');
+    }));
+  }
+}
+
+void main(List<String> arguments) async {
+  // Create actor system
+  var system = ActorSystem('test_system');
+
+  // Initialize actor system before work with it
+  await system.initialize();
+
+  // Create top-level actor in actor system with name 'test_actor'
+  var ref = await system.actorOf('test_actor', TestActor());
+
+  // Send messages
+  Stream.fromIterable(List.generate(5, (index) => index.toString()))
+      .listen((event) {
+    ref.send(event);
+  });
+}
--- lib/src/actor/actor_message_receiver_mixin.dart
+++ lib/src/actor/actor_message_receiver_mixin.dart
@@ -18,4 +18,34 @@ mixin ActorMessageReceiverMixin<P extends ActorProperties> on ActorContext<P> {
 
     return subscription;
   }
+
+  /// Sets [handler] to only for [count] following messages of type [T].
+  ///
+  /// After handled [count] of messages cancels handling messages and completes [Future].
+  Future<void> receiveSeveral<T>(
+      int count, Future<MessageResult?> Function(T) handler) async {
+    var counter = 0;
+
+    var controller = StreamController<MailboxMessage>();
+
+    controller.stream.listen((message) async {
+      var result = await handler(message.data);
+
+      if (message.isHaveSubscription) {
+        result != null ? message.sendResult(result.data) : message.successful();
+      }
+    });
+
+    await for (var message in _messageController.stream) {
+      if (message.data is T) {
+        controller.sink.add(message);
+
+        if (++counter == count) {
+          break;
+        }
+      }
+    }
+
+    await controller.close();
+  }
 }
