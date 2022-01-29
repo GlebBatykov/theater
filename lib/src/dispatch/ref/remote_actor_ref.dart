42013cd9e6e339ed59ec7abeefd47a46018e995c
Gleb Batykov <gleb.batykov@gmail.com>
2022-01-29 18:20:58 +0300

    Updates documentation

--- lib/src/dispatch/ref/remote_actor_ref.dart
+++ lib/src/dispatch/ref/remote_actor_ref.dart
@@ -1,17 +1,17 @@
 part of theater.dispatch;
 
-///
+/// Used to send message to actor wich licated in other actor system.
 class RemoteActorRef extends ActorRef {
-  ///
+  /// Address where the remote actor system is located.
   final dynamic address;
 
-  ///
+  /// Port where the remote actor system is located.
   final int port;
 
   RemoteActorRef(ActorPath path, this.address, this.port, SendPort sendPort)
       : super(path, sendPort);
 
-  ///
+  /// Sends message with [tag].
   void send(String tag, dynamic message, {Duration? delay}) {
     _sendPort.send(ActorRemoteMessage(path, tag, message));
   }
