d4f04426f911cc0973f9f21c6107602a5a77e41a
Gleb Batykov <gleb.batykov@gmail.com>
2022-06-19 01:40:43 +0300

    Adds documentation to actor system builders

--- lib/src/actor_system/builder/actor_system_async_builder.dart
+++ lib/src/actor_system/builder/actor_system_async_builder.dart
@@ -1,7 +1,7 @@
 part of theater.actor_system;
 
-///
+/// Is the base class for builder classes. The logic of asynchronous creating an instance of the actor system is put into such classes.
 abstract class ActorSystemAsyncBuilder {
-  ///
+  /// Asynchronous build instance of [ActorSystem].
   Future<ActorSystem> build();
 }
--- lib/src/actor_system/builder/actor_system_builder.dart
+++ lib/src/actor_system/builder/actor_system_builder.dart
@@ -1,7 +1,7 @@
 part of theater.actor_system;
 
-///
+/// Is the base class for builder classes. The logic of creating an instance of the actor system is put into such classes.
 abstract class ActorSystemBuilder {
-  ///
+  /// Build instance of [ActorSystem].
   ActorSystem build();
 }
