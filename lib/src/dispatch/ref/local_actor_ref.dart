9b8a5b4d0a61264bd30423d1fd593c3c66448ed0
Gleb Batykov <gleb.batykov@gmail.com>
2022-06-19 01:33:01 +0300

    Updates LocalActorRef class
    Adds documentation and removes sendMessage method.

--- lib/src/dispatch/ref/local_actor_ref.dart
+++ lib/src/dispatch/ref/local_actor_ref.dart
@@ -4,22 +4,53 @@ part of theater.dispatch;
 class LocalActorRef extends ActorRef {
   LocalActorRef(ActorPath path, SendPort sendPort) : super(path, sendPort);
 
+  /// Sends message to actor which in located on [path].
+  ///
+  /// If [delay] is not null, message sends after delay equals [delay].
+  ///
+  /// You have two way how point out path to actor:
+  ///
+  /// - relative;
+  /// - absolute.
+  ///
+  /// The relative path is set from current actor.
+  ///
+  /// For example, current actor is "first_actor" - you can point out this path "system/root/user/first_actor/second_actor" like "../second_actor".
+  ///
+  /// Absolute path given by the full path to the actor from the name of the system of actors.
   void send(dynamic message, {Duration? duration}) {
-    if (message is ActorMessage) {
-      throw ActorRefException(
-          message:
-              'message is instance of [ActorMessage], for sending it use [sendMessage] method of [ActorRef].');
+    if (![
+      ActorMailboxMessage,
+      SystemMailboxMessage,
+      ActorRoutingMessage,
+      SystemRoutingMessage
+    ].contains(message.runtimeType)) {
+      message = ActorMailboxMessage(message);
+    }
+
+    if (duration != null) {
+      Future.delayed(duration, () {
+        _sendPort.send(message);
+      });
     } else {
-      if (duration != null) {
-        Future.delayed(duration, () {
-          _sendPort.send(ActorMailboxMessage(message));
-        });
-      } else {
-        _sendPort.send(ActorMailboxMessage(message));
-      }
+      _sendPort.send(message);
     }
   }
 
+  /// Sends message to actor which in located on [path] and return subscribe to message.
+  ///
+  /// If [delay] is not null, message sends after delay equals [delay].
+  ///
+  /// You have two way how point out path to actor:
+  ///
+  /// - relative;
+  /// - absolute.
+  ///
+  /// The relative path is set from current actor.
+  ///
+  /// For example, current actor is "first_actor" - you can point out this path "system/root/user/first_actor/second_actor" like "../second_actor".
+  ///
+  /// Absolute path given by the full path to the actor from the name of the system of actors.
   MessageSubscription sendAndSubscribe(dynamic message, {Duration? duration}) {
     if (message is ActorMessage) {
       throw ActorRefException(
@@ -39,16 +70,6 @@ class LocalActorRef extends ActorRef {
       }
 
       return MessageSubscription(receivePort);
-    }
-  }
-
-  void sendMessage(Message message, {Duration? duration}) {
-    if (duration != null) {
-      Future.delayed(duration, () {
-        _sendPort.send(message);
-      });
-    } else {
-      _sendPort.send(message);
     }
   }
 }
